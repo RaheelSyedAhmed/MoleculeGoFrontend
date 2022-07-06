ca1d3e5cf5161dee9dd980b7edab3f7e2fd36612
RaheelSyedAhmed <RaheelSyedAhmed@users.noreply.github.com>
2022-07-06 02:55:13 -0500

    Established a custom ATP hook
    Made a useATP hook and made it accessible in Profile.js

---
+++ hooks/useATP.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+function useATP(){
+    const [ATP, setATP] = useState(100);
+
+    return {ATP, setATP} ;
+
+}
+export default useATP;
--- screens/CollectionScreens/Profile.js
+++ screens/CollectionScreens/Profile.js
@@ -1,10 +1,17 @@
 
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import useATP from '../../hooks/useATP.js';
 
 const Profile = () => {
+    const ATPHook = useATP();
+    
     return (
         <View style={styles.container}>
             <Text style={styles.headerText}> Profile </Text>
+            <Text> {ATPHook.ATP} </Text>
+            <TouchableOpacity onPress={() => ATPHook.setATP(ATPHook.ATP + 100)}>
+                <Text> Click Me </Text>
+            </TouchableOpacity>
         </View>
     );
 }
