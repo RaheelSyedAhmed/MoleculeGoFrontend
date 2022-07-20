0fb95c7f10122534673351ed1e45d685993d5b8c
RaheelSyedAhmed <RaheelSyedAhmed@users.noreply.github.com>
2022-07-20 11:55:18 -0500

    Fixed paths with local path
    Updated molecule_paths.py to utilize local path instead of thumbnail fn to discover the appropriate file names.

--- molecule_paths.py
+++ molecule_paths.py
@@ -7,13 +7,10 @@ with open('./data/base.json', 'r') as base:
 parsed_file = json.loads(f)
 
 
-
 count = 0
-base_thumbnails = []
 for base in parsed_file:
     count += 1
     if count > 20 and str(base["thumbnail_fn"]).strip() != "": # Skips amino acids and non-thumbnail bases
-        base_thumbnails.append(base['thumbnail_fn'])
-        message = str(base['pk']).replace("-","") + ": require(\"../assets/molecule_images/BASE_" + base["thumbnail_fn"] + "\"),"
-        #print(message)
-print(sorted(base_thumbnails))
+        
+        message = str(base['pk']).replace("-","") + ": require(\"../assets/molecule_images/" + base["local_path"] + "\"),"
+        print(message)
